52f482bae5aa88f68745b75ab8e398f8eb7c62af
Torikul <toriqulislam142@gmail.com>
2022-06-09 00:16:08 -0500

    header completed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
 import ContactMe from './components/ContactMe/ContactMe'
+import Projects from './components/Projects/Projects';
+import Education from './components/Education/Education';
 
 function App() {
   return (
     <>
       <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='contactMe' element={<ContactMe/>} />
-
+        <Route path='/contactMe' element={<ContactMe />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/education' element={<Education />} />
       </Routes>
     
     
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,14 @@
 import React from 'react';
+import Header from './Header/Header'
 import './Home.css'
+
+
 const Home = () => {
+  
     return (
-        <header>
-            <nav>
-                <div><h1 className='logo'>TORIKUL ISLAM</h1></div>
-                <div className='links'>
-                    <a href="/">Home</a>
-                    <a href="/">Education</a>
-                    <a href="/">Projcts</a>
-                </div>
-                <div>
-                    <button className='contact-button'>Contact Me</button>
-                </div>
-            </nav>
-
-
-        </header>
+        <>
+        <Header />
+        </>
     );
 };
 
