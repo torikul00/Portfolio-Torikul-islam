fa71329db230fb09102e19ec33a6839be1461d30
Torikul islam <toriqulislam142@gmail.com>
2022-10-04 05:08:44 -0500

    get to me know section added

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,7 +16,7 @@ const Main = () => {
                     <a href="">ABOUT</a>
                     <a href="">PROJECT</a>
                     <a href="">CONTACT</a>
-                   <a target='blank' href="https://drive.google.com/file/d/1yixxhzTJXkMvLhyyWtxX88osKB1tl_Sg/view"> <button className='resume-button'>CHECK RESUME</button></a>
+                    <a target='blank' href="https://drive.google.com/file/d/1yixxhzTJXkMvLhyyWtxX88osKB1tl_Sg/view"> <button className='resume-button'>CHECK RESUME</button></a>
                 </div>
             </nav>
             <main>
@@ -37,11 +37,39 @@ const Main = () => {
                     </div>
                 </header>
                 <section className='about-container'>
-                <div className="about-content">
-                <h1>  ABOUT ME</h1>
-                  <div className="line"></div>
-                  <p>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
-                </div>
+                    <div className="about-content">
+                        <h1>  ABOUT ME</h1>
+                        <div className="line"></div>
+                        <p>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
+                    </div>
+                </section>
+
+
+                <section className='know-me-container'>
+                    <div className='know-me-desc'>
+                        <h1>Get to know me !</h1>
+                        <p>I'm a Frontend Web Developer building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section. <br /> <br />
+
+                            I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community. Feel free to Connect or Follow me on my <a className='linkedin' href="https://www.linkedin.com/in/torikul1/">Linkedin</a> where I post useful content related to Web Development and Programming <br /> <br />
+
+                            I'm open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to contact me.</p>
+                           <a href=""> <button className='contact-button'>CONTACT</button></a>
+                    </div>
+                    <div className='skills-container'>
+                        <h1>MY SKILLS</h1>
+                       <div className="skill-buttons">
+                       <button>HTML</button>
+                        <button>CSS</button>
+                        <button>JavaScript</button>
+                        <button>Tailwind CSS</button>
+                        <button>React JS</button>
+                        <button>Node JS</button>
+                        <button>GitHub</button>
+                        <button>MongoDB</button>
+                        <button>Express JS</button>
+                        <button>Bootstrap</button>
+                       </div>
+                    </div>
                 </section>
             </main>
             <div className="chat">
